04596321ea42d92e2deb27fda2dbb1fd78500feb
halopeter <flamesea02@gmail.com>
2018-11-20 17:18:55 +0100

    TypeO fix

--- notebook-service-frontend/src/App.js
+++ notebook-service-frontend/src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react;
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
-    return null;
+    return 0;
   }
 }
 
