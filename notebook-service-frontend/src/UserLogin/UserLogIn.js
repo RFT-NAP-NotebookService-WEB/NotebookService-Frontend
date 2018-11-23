3598d21659c67864383cf137c9750361015ece70
Miki <b.miki517@gmail.com>
2018-11-24 00:05:38 +0100

    Login screen design

--- notebook-service-frontend/src/UserLogin/UserLogIn.js
+++ notebook-service-frontend/src/UserLogin/UserLogIn.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Modal } from 'react-bootstrap';
-//import Col from 'react-bootstrap/lib/Col';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
-import FormControl from 'react-bootstrap/lib/FormControl';
-import Checkbox from 'react-bootstrap/lib/Checkbox';
-import Button from 'react-bootstrap/lib/Button';
-import Image from 'react-bootstrap/lib/Image';
+import {FormGroup ,FormControl, Checkbox,Button,Image} from 'react-bootstrap/lib';
+
 
 import './UserLogin.css';
 
@@ -17,19 +13,19 @@ const UserLogin = () => {
 
         <Modal.Dialog bsSize="small">
             <Modal.Header >
+                <Image src="https://cdn.pixabay.com/photo/2017/03/08/14/20/flat-2126880_960_720.png" rounded />
                 <Modal.Title>Notebook-service</Modal.Title>
-                <Image src="/public/assets/loginPicture.png" rounded />
             </Modal.Header>
             <Modal.Body>
                 <FormGroup className="UserDataContainer">
-                    <FormControl type="username" placeholder="Username" />
-                    <FormControl type="password" placeholder="Password" />
+                    <FormControl bsSize="large" type="username" placeholder="Username" />
+                    <FormControl bsSize="large" type="password" placeholder="Password" />
                 </FormGroup>
             </Modal.Body>
             <Modal.Footer>
                 <FormGroup className="ButtonContainer">
-                    <Button type="login">Log in</Button>
-                    <Button type="signin">Sign in</Button>
+                    <Button bsSize="large" type="submit">Log in</Button>
+                    <Button bsSize="large" type="button">Sign in</Button>
                     <Checkbox>Remember me</Checkbox>
                 </FormGroup>
             </Modal.Footer>
