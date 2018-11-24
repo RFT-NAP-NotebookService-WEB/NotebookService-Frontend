b325f790b127fb1df7fb1207e2e8ade3d3af85bb
halopeter <flamesea02@gmail.com>
2018-11-24 20:12:10 +0100

    Service page v1.0

--- notebook-service-frontend/src/App.js
+++ notebook-service-frontend/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import './UserLogin/UserLogIn';
 import UserLogin from './UserLogin/UserLogIn';
+import Service from './Service/Service';
 
 class App extends Component {
   render() {
     return (
       <div>
         <UserLogin />
+        <Service />
       </div>
     )
   }
